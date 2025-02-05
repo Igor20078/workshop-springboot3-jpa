4b86eadcb40c02503c49f6abefce5ba1c6a864f9
Igor20078 <igormacedoferreira70@gmail.com>
2025-02-05 00:00:05 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,20 +1,16 @@
 package com.educandoweb.course.resources;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.educandoweb.course.entities.User;
-
 @RestController
-@RequestMapping(value = "/users")
-public class UserResource {
+@RequestMapping("/")
+public class UserResource  {
 
-	
-	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
-		return ResponseEntity.ok().body(u);
-	}
+    @GetMapping
+    public String home() {
+        return "Aplicação está rodando!";
+    }
 }
+
